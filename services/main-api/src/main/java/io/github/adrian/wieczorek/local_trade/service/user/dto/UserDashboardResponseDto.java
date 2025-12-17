1450fed247955b0ea2bf7cddc36e11e95eec3fca
Czjena <wieczorek.adrian12@gmail.com>
2025-12-17 21:45:28 +0100

    refactor: change UserDashBoardDto response

--- services/main-api/src/main/java/io/github/adrian/wieczorek/local_trade/service/user/dto/UserDashboardResponseDto.java
+++ services/main-api/src/main/java/io/github/adrian/wieczorek/local_trade/service/user/dto/UserDashboardResponseDto.java
@@ -1,5 +1,11 @@
 package io.github.adrian.wieczorek.local_trade.service.user.dto;
 
+import java.util.UUID;
 
-public record UserDashboardResponseDto(String email, int ratingCount, double averageRating)  {
+public record UserDashboardResponseDto(String email,
+                                       int ratingCount,
+                                       double averageRating,
+                                       String role,
+                                       UUID userId,
+                                       String name)  {
 }
