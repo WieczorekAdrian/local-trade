c057a3ddeee29d4607cc4bfeaf0e08c3c2efb77d
Czjena <wieczorek.adrian12@gmail.com>
2026-01-12 00:35:03 +0100

    refactor: change remaining files to match cqs and fix favorite count

--- services/main-api/src/main/java/io/github/adrian/wieczorek/local_trade/service/advertisement/service/FavoriteAdvertisementServiceImpl.java
+++ services/main-api/src/main/java/io/github/adrian/wieczorek/local_trade/service/advertisement/service/FavoriteAdvertisementServiceImpl.java
@@ -1,44 +1,30 @@
 package io.github.adrian.wieczorek.local_trade.service.advertisement.service;
 
-import io.github.adrian.wieczorek.local_trade.service.advertisement.dto.FavoriteAdvertisementDto;
-import io.github.adrian.wieczorek.local_trade.exceptions.UserNotFoundException;
 import io.github.adrian.wieczorek.local_trade.service.advertisement.mapper.FavoriteAdvertisementMapper;
 import io.github.adrian.wieczorek.local_trade.service.advertisement.AdvertisementEntity;
 import io.github.adrian.wieczorek.local_trade.service.user.UsersEntity;
 import io.github.adrian.wieczorek.local_trade.service.advertisement.AdvertisementRepository;
-import io.github.adrian.wieczorek.local_trade.service.user.UsersRepository;
+import io.github.adrian.wieczorek.local_trade.service.user.service.UsersService;
 import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Set;
 import java.util.UUID;
-import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class FavoriteAdvertisementServiceImpl implements FavoriteAdvertisementService {
 
   private final AdvertisementRepository advertisementRepository;
-  private final UsersRepository usersRepository;
   private final FavoriteAdvertisementMapper favoriteAdvertisementMapper;
-
-  @Override
-  @Transactional(readOnly = true)
-  public Set<FavoriteAdvertisementDto> getFavoriteAdvertisements(UserDetails userDetails) {
-    return usersRepository.findByEmail(userDetails.getUsername())
-        .map(users -> users.getFavoritedAdvertisementEntities().stream()
-            .map(favoriteAdvertisementMapper::toDto).collect(Collectors.toSet()))
-        .orElseThrow(() -> new UserNotFoundException(
-            "No user found with username: " + userDetails.getUsername()));
-  }
+  private final UsersService usersService;
 
   @Transactional
   @Override
-  public void addFavoriteAdvertisement(UserDetails userDetails, UUID advertisementId) {
-    UsersEntity user = getUser(userDetails);
+  public void addFavoriteAdvertisement(String email, UUID advertisementId) {
+    UsersEntity user = usersService.getCurrentUser(email);
     AdvertisementEntity ad = advertisementRepository.findByAdvertisementId(advertisementId)
         .orElseThrow(() -> new EntityNotFoundException("Advertisement not found "));
     Set<UsersEntity> favoritedByUsers = ad.getFavoritedByUsers();
@@ -47,27 +33,18 @@ public class FavoriteAdvertisementServiceImpl implements FavoriteAdvertisementSe
     favoritedByUsers.add(user);
     favoritedByAdvertisementEntity.add(ad);
     advertisementRepository.save(ad);
-    usersRepository.save(user);
+    usersService.saveUser(user);
   }
 
   @Override
   @Transactional
-  public void deleteFavoriteAdvertisement(UserDetails userDetails, UUID advertisementId) {
-    UsersEntity user = getUser(userDetails);
+  public void deleteFavoriteAdvertisement(String email, UUID advertisementId) {
+    UsersEntity user = usersService.getCurrentUser(email);
     AdvertisementEntity ad = advertisementRepository.findByAdvertisementId(advertisementId)
         .orElseThrow(() -> new EntityNotFoundException("Advertisement not found "));
     ad.getFavoritedByUsers().remove(user);
     user.getFavoritedAdvertisementEntities().remove(ad);
     advertisementRepository.save(ad);
-    usersRepository.save(user);
+    usersService.saveUser(user);
   }
-
-  @Override
-  @Transactional(readOnly = true)
-  public UsersEntity getUser(UserDetails userDetails) {
-    return usersRepository.findByEmail(userDetails.getUsername())
-        .orElseThrow(() -> new UserNotFoundException(
-            "No user found with username: " + userDetails.getUsername()));
-  }
-
 }
--- services/main-api/src/test/java/io/github/adrian/wieczorek/local_trade/unit/FavoriteAdvertisementEntityTests.java
+++ services/main-api/src/test/java/io/github/adrian/wieczorek/local_trade/unit/FavoriteAdvertisementEntityTests.java
@@ -4,8 +4,8 @@ import io.github.adrian.wieczorek.local_trade.exceptions.UserNotFoundException;
 import io.github.adrian.wieczorek.local_trade.service.advertisement.AdvertisementEntity;
 import io.github.adrian.wieczorek.local_trade.service.user.UsersEntity;
 import io.github.adrian.wieczorek.local_trade.service.advertisement.AdvertisementRepository;
-import io.github.adrian.wieczorek.local_trade.service.user.UsersRepository;
 import io.github.adrian.wieczorek.local_trade.service.advertisement.service.FavoriteAdvertisementServiceImpl;
+import io.github.adrian.wieczorek.local_trade.service.user.service.UsersService;
 import io.github.adrian.wieczorek.local_trade.testutils.AdUtils;
 import io.github.adrian.wieczorek.local_trade.testutils.UserUtils;
 import jakarta.persistence.EntityNotFoundException;
@@ -15,7 +15,6 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Optional;
 
@@ -26,7 +25,7 @@ import static org.mockito.Mockito.*;
 public class FavoriteAdvertisementEntityTests {
 
   @Mock
-  UsersRepository usersRepository;
+  UsersService usersService;
   @Mock
   AdvertisementRepository advertisementRepository;
   @InjectMocks
@@ -36,13 +35,11 @@ public class FavoriteAdvertisementEntityTests {
   public void whenAddingFavoriteAdvertisement_thenSuccess() {
     UsersEntity user = UserUtils.createUserRoleUser();
     AdvertisementEntity ad = AdUtils.createAdvertisement();
-    UserDetails mockUserDetails = mock(UserDetails.class);
-    when(mockUserDetails.getUsername()).thenReturn(user.getEmail());
-    when(usersRepository.findByEmail(user.getEmail())).thenReturn(Optional.of(user));
+    when(usersService.getCurrentUser(user.getEmail())).thenReturn(user);
     when(advertisementRepository.findByAdvertisementId(ad.getAdvertisementId()))
         .thenReturn(Optional.of(ad));
 
-    favoriteAdvertisementService.addFavoriteAdvertisement(mockUserDetails, ad.getAdvertisementId());
+    favoriteAdvertisementService.addFavoriteAdvertisement(user.getEmail(), ad.getAdvertisementId());
     verify(advertisementRepository).save(ad);
     assertTrue(ad.getFavoritedByUsers().contains(user));
     assertEquals(1, ad.getFavoritedByUsers().size());
@@ -53,12 +50,10 @@ public class FavoriteAdvertisementEntityTests {
     UsersEntity user = UserUtils.createUserRoleUser();
     user.setName("user");
     AdvertisementEntity ad = AdUtils.createAdvertisement();
-    UserDetails mockUserDetails = mock(UserDetails.class);
-    when(mockUserDetails.getUsername()).thenReturn(user.getEmail());
-    when(usersRepository.findByEmail(user.getEmail())).thenReturn(Optional.empty());
+    when(usersService.getCurrentUser(user.getEmail())).thenThrow(UserNotFoundException.class);
 
     assertThrows(UserNotFoundException.class, () -> {
-      favoriteAdvertisementService.addFavoriteAdvertisement(mockUserDetails,
+      favoriteAdvertisementService.addFavoriteAdvertisement(user.getEmail(),
           ad.getAdvertisementId());
     });
     verify(advertisementRepository, never()).save(any(AdvertisementEntity.class));
@@ -68,14 +63,12 @@ public class FavoriteAdvertisementEntityTests {
   public void whenAddingFavoriteAdvertisement_thenAdvertisementNotFound() {
     UsersEntity user = UserUtils.createUserRoleUser();
     AdvertisementEntity ad = AdUtils.createAdvertisement();
-    UserDetails mockUserDetails = mock(UserDetails.class);
-    when(mockUserDetails.getUsername()).thenReturn(user.getEmail());
-    when(usersRepository.findByEmail(user.getEmail())).thenReturn(Optional.of(user));
+    when(usersService.getCurrentUser(user.getEmail())).thenReturn(user);
     when(advertisementRepository.findByAdvertisementId(ad.getAdvertisementId()))
         .thenReturn(Optional.empty());
 
     assertThrows(EntityNotFoundException.class, () -> {
-      favoriteAdvertisementService.addFavoriteAdvertisement(mockUserDetails,
+      favoriteAdvertisementService.addFavoriteAdvertisement(user.getEmail(),
           ad.getAdvertisementId());
     });
     verify(advertisementRepository, never()).save(any(AdvertisementEntity.class));
@@ -92,13 +85,11 @@ public class FavoriteAdvertisementEntityTests {
     assertEquals(1, ad.getFavoritedByUsers().size());
     assertTrue(ad.getFavoritedByUsers().contains(user));
 
-    when(usersRepository.findByEmail(user.getEmail())).thenReturn(Optional.of(user));
+    when(usersService.getCurrentUser(user.getEmail())).thenReturn(user);
     when(advertisementRepository.findByAdvertisementId(ad.getAdvertisementId()))
         .thenReturn(Optional.of(ad));
-    UserDetails mockUserDetails = mock(UserDetails.class);
-    when(mockUserDetails.getUsername()).thenReturn(user.getEmail());
 
-    favoriteAdvertisementService.deleteFavoriteAdvertisement(mockUserDetails,
+    favoriteAdvertisementService.deleteFavoriteAdvertisement(user.getEmail(),
         ad.getAdvertisementId());
     verify(advertisementRepository).save(ad);
     Assertions.assertFalse(ad.getFavoritedByUsers().contains(user));
@@ -110,12 +101,10 @@ public class FavoriteAdvertisementEntityTests {
     UsersEntity user = UserUtils.createUserRoleUser();
     AdvertisementEntity ad = AdUtils.createAdvertisement();
 
-    when(usersRepository.findByEmail(user.getEmail())).thenReturn(Optional.empty());
-    UserDetails mockUserDetails = mock(UserDetails.class);
-    when(mockUserDetails.getUsername()).thenReturn(user.getEmail());
+    when(usersService.getCurrentUser(user.getEmail())).thenThrow(UserNotFoundException.class);
 
     assertThrows(UserNotFoundException.class, () -> {
-      favoriteAdvertisementService.deleteFavoriteAdvertisement(mockUserDetails,
+      favoriteAdvertisementService.deleteFavoriteAdvertisement(user.getEmail(),
           ad.getAdvertisementId());
     });
     verify(advertisementRepository, never()).save(any(AdvertisementEntity.class));
@@ -126,14 +115,12 @@ public class FavoriteAdvertisementEntityTests {
     UsersEntity user = UserUtils.createUserRoleUser();
     AdvertisementEntity ad = AdUtils.createAdvertisement();
 
-    when(usersRepository.findByEmail(user.getEmail())).thenReturn(Optional.of(user));
+    when(usersService.getCurrentUser(user.getEmail())).thenReturn(user);
     when(advertisementRepository.findByAdvertisementId(ad.getAdvertisementId()))
         .thenReturn(Optional.empty());
-    UserDetails mockUserDetails = mock(UserDetails.class);
-    when(mockUserDetails.getUsername()).thenReturn(user.getEmail());
 
     assertThrows(EntityNotFoundException.class, () -> {
-      favoriteAdvertisementService.deleteFavoriteAdvertisement(mockUserDetails,
+      favoriteAdvertisementService.deleteFavoriteAdvertisement(user.getEmail(),
           ad.getAdvertisementId());
     });
     verify(advertisementRepository, never()).save(any(AdvertisementEntity.class));
