0947bd0386c4d05f741be3b6659b032350e81784
Czjena <wieczorek.adrian12@gmail.com>
2026-01-15 02:38:31 +0100

    fix: flaky test for WebChatFinder

--- services/main-api/src/test/java/io/github/adrian/wieczorek/local_trade/integration/AbstractIntegrationTest.java
+++ services/main-api/src/test/java/io/github/adrian/wieczorek/local_trade/integration/AbstractIntegrationTest.java
@@ -1,14 +1,33 @@
 
 package io.github.adrian.wieczorek.local_trade.integration;
 
+import io.github.adrian.wieczorek.local_trade.service.advertisement.AdvertisementRepository;
+import io.github.adrian.wieczorek.local_trade.service.category.CategoryRepository;
+import io.github.adrian.wieczorek.local_trade.service.chat.ChatMessageRepository;
+import io.github.adrian.wieczorek.local_trade.service.user.UsersRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 
+import java.util.List;
+
 @SpringBootTest
 @ActiveProfiles("test")
 public abstract class AbstractIntegrationTest extends SharedContainerInitializer {
+
+    @Autowired
+    ChatMessageRepository chatMessageRepository;
+    @Autowired
+    UsersRepository usersRepository;
+    @Autowired
+    AdvertisementRepository advertisementRepository;
+    @Autowired
+    CategoryRepository categoryRepository;
 
   @DynamicPropertySource
   static void registerPgProperties(DynamicPropertyRegistry registry) {
@@ -18,5 +37,5 @@ public abstract class AbstractIntegrationTest extends SharedContainerInitializer
     registry.add("spring.data.redis.host", REDIS_CONTAINER::getHost);
     registry.add("spring.data.redis.port", () -> REDIS_CONTAINER.getMappedPort(6379).toString());
   }
-
 }
+
