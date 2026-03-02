10207410726131c7324c7504fa4abd02a8460900
Czjena <wieczorek.adrian12@gmail.com>
2026-03-02 01:36:06 +0100

    fix: ws heartbeat problem fix with env for azure

--- services/main-api/src/main/java/io/github/adrian/wieczorek/local_trade/configs/WebSocketConfiguration.java
+++ services/main-api/src/main/java/io/github/adrian/wieczorek/local_trade/configs/WebSocketConfiguration.java
@@ -1,9 +1,11 @@
 package io.github.adrian.wieczorek.local_trade.configs;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.lang.NonNull;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.scheduling.TaskScheduler;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
@@ -13,30 +15,35 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 @EnableWebSocketMessageBroker
 public class WebSocketConfiguration implements WebSocketMessageBrokerConfigurer {
 
-    private final CookieHandshakeInterceptor cookieHandshakeInterceptor;
+  @Bean
+  public TaskScheduler customWebSocketTaskScheduler() {
+    ThreadPoolTaskScheduler scheduler = new ThreadPoolTaskScheduler();
+    scheduler.setPoolSize(1);
+    scheduler.setThreadNamePrefix("ws-heartbeat-thread-");
+    return scheduler;
+  }
 
-    @Value("${allowed.websocket.origins}")
-    private String allowedWebsocketOrigins;
+  private final CookieHandshakeInterceptor cookieHandshakeInterceptor;
 
-    public WebSocketConfiguration(CookieHandshakeInterceptor cookieHandshakeInterceptor) {
-        this.cookieHandshakeInterceptor = cookieHandshakeInterceptor;
-    }
+  @Value("${allowed.websocket.origins}")
+  private String allowedWebsocketOrigins;
 
-    @Override
-    public void configureMessageBroker(@NonNull MessageBrokerRegistry registry) {
-        registry.setApplicationDestinationPrefixes("/app");
-        registry.enableSimpleBroker("/topic", "/queue")
-                .setTaskScheduler(new ThreadPoolTaskScheduler())
-                .setHeartbeatValue(new long[]{10000, 10000});
+  public WebSocketConfiguration(CookieHandshakeInterceptor cookieHandshakeInterceptor) {
+    this.cookieHandshakeInterceptor = cookieHandshakeInterceptor;
+  }
 
-        registry.setUserDestinationPrefix("/user");
-    }
+  @Override
+  public void configureMessageBroker(@NonNull MessageBrokerRegistry registry) {
+    registry.setApplicationDestinationPrefixes("/app");
+    registry.enableSimpleBroker("/topic", "/queue").setTaskScheduler(customWebSocketTaskScheduler())
+        .setHeartbeatValue(new long[] {10000, 10000});
+    registry.setUserDestinationPrefix("/user");
+  }
 
-    @Override
-    public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws")
-                .setAllowedOriginPatterns(allowedWebsocketOrigins.split(","))
-                .addInterceptors(cookieHandshakeInterceptor)
-                .setHandshakeHandler(new CustomHandshakeHandler());
-    }
+  @Override
+  public void registerStompEndpoints(StompEndpointRegistry registry) {
+    registry.addEndpoint("/ws").setAllowedOriginPatterns(allowedWebsocketOrigins.split(","))
+        .addInterceptors(cookieHandshakeInterceptor)
+        .setHandshakeHandler(new CustomHandshakeHandler());
+  }
 }
